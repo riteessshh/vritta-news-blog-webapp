130d1d0be372771bd8f28b6ff7b138a69f2c28f0
Ritesh Kumar <74615197+riteessshh@users.noreply.github.com>
2024-06-21 18:01:17 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,19 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.css";
 import Home from "./components/Home";
 import Page from "./components/Page";
-import "./App.css";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/article/:articleId" element={<Page />} />
-      </Routes>
-    </BrowserRouter>
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/article/:articleId" element={<Page />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
   );
 }
 
