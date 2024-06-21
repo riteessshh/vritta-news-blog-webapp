cb4592abcf1e174b5e64706d0c0295ef532971e8
Ritesh Kumar <74615197+riteessshh@users.noreply.github.com>
2024-06-21 17:26:50 +0530

    static json file added

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,15 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./components/home";
-import "./App.css";
 import Page from "./components/Page";
+import "./App.css";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route
-          path="/article/:articleId"
-          element={<Page />}
-          key={Math.round(Math.random() * 10)}
-        />
+        <Route path="/article/:articleId" element={<Page />} />
       </Routes>
     </BrowserRouter>
   );
